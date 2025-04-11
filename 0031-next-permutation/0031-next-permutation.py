1487267abca68f3c18e5b95260293be10382be8f
Muneera <muneerashaik03@gmail.com>
2025-04-11 20:10:53 +0530

    Time: 0 ms (100%), Space: 12.5 MB (53.14%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def nextPermutation (self, nums) :
+        n = len(nums)
+        ind = -1
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1] :
+                ind = i
+                break
+
+        if ind == -1:
+            nums.reverse()
+            return nums
+
+        for i in range(n-1,ind,-1):
+            if nums[i] > nums[ind]:
+                nums[i],nums[ind] = nums[ind],nums[i]
+                break
+        nums[ind+1:] = reversed(nums[ind+1:])
+
+obj = Solution()
+nums= [1,2,3]
+obj.nextPermutation(nums)
+        
+        
